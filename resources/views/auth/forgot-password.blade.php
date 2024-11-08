cb8219d51575ee2c429f34c3fd126684a05bf4c8
Kaross7 <user.email>
2024-11-08 11:28:24 +0100

    login en register aangemaakt

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,6 +1,6 @@
 <x-guest-layout>
     <div class="mb-4 text-sm text-gray-600">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+        {{ __('Wachtwoord vergeten? Geen probleem. Laat ons je e-mailadres weten, dan sturen we je een link om je wachtwoord te resetten. Hiermee kun je een nieuw wachtwoord kiezen.') }}
     </div>
 
     <!-- Session Status -->
@@ -18,7 +18,7 @@
 
         <div class="flex items-center justify-end mt-4">
             <x-primary-button>
-                {{ __('Email Password Reset Link') }}
+                {{ __('E-mail Wachtwoord Reset Link') }}
             </x-primary-button>
         </div>
     </form>
--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
                 'resources/css/app.css',    
                 'resources/js/app.js',      
                 'resources/css/begin.css', 
+                'resources/js/login_register.js'
             ],
             refresh: true,
         }),
